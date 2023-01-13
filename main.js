7fc84b7402835093c46258193ba6a1d2eba232fe
aseedee <64454213+aseedee@users.noreply.github.com>
2023-01-14 00:38:25 +0300

    Убрали "Бебропад" т.к. его больше нет

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ function main() {
         {id: 'l_ads', title: 'Бебреклама'},
         {id: 'l_apm', title: 'Бебравление'},
         {id: 'l_ca', title: 'Бебрафон'},
-        {id: 'l_snowflake', title: 'Бебропад'}
     ];
     replacements.forEach(({ id, title }) => {
         document.querySelector('#' + id + ' ' +
